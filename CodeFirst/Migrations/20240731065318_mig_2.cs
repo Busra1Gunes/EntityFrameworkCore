1595a955db8d08035a78dca851903ed9f39f27fe
Busra1Gunes <busragns1999@hotmail.com>
2024-07-31 10:35:03 +0300

    Migrate etme

---
+++ CodeFirst/Migrations/20240731065318_mig_2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Price",
+                table: "Products",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+    }
+}
---
+++ CodeFirst/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+//Kod üzerinden migrate işlemi
+//ECommerceDbContext context = new();
+//await context.Database.MigrateAsync();
+
+
+//ExampleDb, DbContext'ten türeyen bir sınıf olduğu için veritabanı sunucusunda bir veritabanına karşılık gelen sınıf olduğu bilinir
+public class ECommerceDbContext:DbContext
+{
+    //Tablo oluşturma
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Customer> Customers { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=AYZASOFT-0000;Database=ECommerce;Integrated Security=True;Trust Server Certificate=True");
+    }
+}
+
+//Entity
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
